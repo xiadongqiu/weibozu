d307f141a871d0b8e63dc3a1d748c29eb4524768
xdq <714544385@qq.com>
2018-01-10 11:30:26 +0800

    mergin chenm

--- weibo/app/Http/routes.php
+++ weibo/app/Http/routes.php
@@ -50,72 +50,48 @@ Route::group(['prefix'=>'user','namespace'=>'Home'],function(){
 
 });
 //===============================================================
+
+
 //后台
 Route::group(['prefix' => 'admin','namespace' => 'admin'], function () {
-	Route::get('/login','LoginController@index');
-	Route::post('/login','LoginController@login');
+
+  //跳转到登录页
+  Route::get('/login','LoginController@index');
+
+  //登录提交执行
+  Route::post('/login','LoginController@login');
+
+  //判断是否登录的中间件
+  Route::group(['middleware' => 'adminlogin'], function () {
+
+    //跳转到首页
+    Route::get('/','IndexController@index');
+    Route::get('/index','IndexController@index');
+
+    //退出登录
+    Route::get('/loginout','LoginController@loginout');
+
+     //默认跳转到用户list
+    Route::get('/user',function(){return redirect('admin/user/list');});
+    Route::resource('/user/list','UserController');
 
 
-	Route::group(['middleware' => 'login'], function () {
-	    Route::get('/','IndexController@index');
-	});
+    //默认跳转到举报list
+    Route::get('/report',function(){return redirect('admin/report/list');});
+    Route::resource('/report/list','ReportController');
+   });
+   
+
+
+//吕明泽路由=====================================================================
+    Route::resource('/post','PostController');
+    Route::resource('/comments','CommentsController');
+//==============================================================================
+
+
+//郑鑫路由=======================================================================
+    //后台网站配置路由
+     Route::get('/config','ConfigController@edit');
+//==============================================================================
 });
-
-	//Route::group(['middleware' => 'login'], function () {
-        Route::resource('/post','PostController');
-           
-    // });
-
-
-
-
-  	//判断是否登录的中间件
-		Route::group(['middleware' => 'login'], function () {
-
-  		//判断是否登录的中间件
-		Route::group(['middleware' => 'adminlogin'], function () {
-
-
-
-  		//判断是否登录的中间件
-		// Route::group(['middleware' => 'login'], function () {
-
-
-
-
-    
-
-
-
-  		//判断是否登录的中间件
-		Route::group(['middleware' => 'login'], function () {
-
-   			Route::get('/','IndexController@index');
-   			Route::get('/index','IndexController@index');
-   			Route::get('/loginout','LoginController@loginout');
-
-        //默认跳转到用户list
-        Route::get('/user',function(){return redirect('admin/user/list');});
-   			Route::resource('/user/list','UserController');
-
-
-
-        //默认跳转到举报list
-        Route::get('/report',function(){return redirect('admin/report/list');});
-        Route::resource('/report/list','ReportController');
-  	  	});
-
-
-  	  	// });
-        Route::resource('/post','PostController');
-        //后台网站配置路由
-        Route::get('/config','ConfigController@edit');
-});
-
-
-  	  	// });
-  		Route::resource('/post','PostController');
-  		Route::resource('/comments','CommentsController');
-
-});
-
+      
