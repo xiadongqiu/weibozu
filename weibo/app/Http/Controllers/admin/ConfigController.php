133fa7a2365a4be7dd6e4223927e8d6ab0f76eb5
zhengxin <497603694@qq.com>
2018-01-22 08:54:53 +0800

    a

--- weibo/app/Http/Controllers/admin/ConfigController.php
+++ weibo/app/Http/Controllers/admin/ConfigController.php
@@ -14,10 +14,26 @@ class ConfigController extends Controller
     public function edit()
     {
     	//获取 网站配置
-    	// $config = config::first();
+    	$config = config::where('status','1')->first();
 
-    	return view('admin/config/edit');
-    	//
+    	return view('admin/config/edit',['config'=>$config]);  	
+    }
+
+    //执行修改功能
+    public function update(Request $request)
+    {
+    	//获取修改后的信息
+    	$config = $request->except('_token');
+    	//修改数据库
+    	$data = config::where('id',1)->update($config);
+    	
+    	 if($data){
+            return redirect('/admin/config/')->with('msg','网站配置修改成功！');
+        } else {
+            return redirect('/admin/config/')->with('msg','网站配置修改失败！');
+
+        }  
 
     }
+
 }
